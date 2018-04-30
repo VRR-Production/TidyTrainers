c4a9423e8b5d30f26754373c2816ac138245a93c
Student <Student@aii.stu>
2018-04-30 16:57:50 -0700

    Updated file structure and added template scripts

---
+++ Westgate_VR_Training_Simulator/Assets/Scripts/Master Scripts/Template_Master.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WestgateVRTS{
+
+    public class Template_Master : MonoBehaviour{
+
+        // the delegate is an event type that you will create your specific events to, the delegate is a way to stage events to happen when you call them...
+        public delegate void GeneralEventHandler();
+        // the event is of the delegate type, then use Event in the name of the event
+        public event GeneralEventHandler EventSomethingSomethingDarkSide;
+
+        public delegate void OtherEventHandler();
+        public event OtherEventHandler EventSomethingSomethingElse;
+        
+        //you can require a parameter in the delgate but not in the event, however when you create the function later, because the delegate
+        //requires the parameter, the function will require it as well!
+        public delegate void PlayerHealthEventHandler(int change);
+        public event PlayerHealthEventHandler EventPlayerHealthDeduction;
+        public event PlayerHealthEventHandler EventPlayerHealthIncrease;
+
+
+        //now we create a call function of each event written above. best layout is to do it in the order you made them above...
+        // note the Call keyword before the name of the function, this is because this isnt an event, its a function that calls the event.
+        public void CallEventSomethingSomethingDarkSide() {
+            //inside the function, we check if the event isnt empty..basically were checking if anything has subscribed to this event. if not, carryon...
+            if (EventSomethingSomethingDarkSide != null) {
+                //now here is where we finally call our event. anything subscribed to this even will get called when this function is called anywhere in any script.
+                EventSomethingSomethingDarkSide();
+            }
+        }
+
+        public void CallEventSomethingSomethingElse() {
+            if (EventSomethingSomethingElse != null) {
+                EventSomethingSomethingElse();
+            }
+        }
+
+        //this is a demonstration where the delegate requires a parameter be passed in.
+        public void CallEventPlayerHealthDeduction(int healthDeductionAmount) {
+            if (EventPlayerHealthDeduction != null) {
+                //uncomment the code below this line, and see how it throws an error because its missing its parameters...
+                //EventPlayerHealthDeduction();
+                EventPlayerHealthDeduction(healthDeductionAmount);
+            }
+        }
+
+        public void CallEventPlayerHealthIncrease(int healthIncreaseAmount) {
+            if (EventPlayerHealthIncrease != null) {
+                EventPlayerHealthIncrease(healthIncreaseAmount);
+            }
+        }
+
+    }
+}
---
+++ Westgate_VR_Training_Simulator/Assets/Scripts/Template Scripts/Template_DoSomething.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WestgateVRTS{
+    public class Template_DoSomething : MonoBehaviour{
+
+
+        //first we set up the master script variable...
+        private Template_Master templateMaster;
+        //then create any other variables you may need
+        public int damage = 10;
+        public GameObject player;
+        int playerHealth;
+
+        //we use on enable because its efficient and works well with turning objects on and off, and helps us not need update.
+        private void OnEnable(){
+            //call our initial references setup function...
+            InitRef();
+            //this is a subscription. were taking the DoSomeDamge function, and appending it to the EventPlayerHealthDeduction event
+            templateMaster.EventPlayerHealthDeduction += DoSomeDamage;
+        }
+
+        private void OnDisable(){
+            //if we subscribe, we always have to unsubscribe.. this is a must.
+            templateMaster.EventPlayerHealthDeduction -= DoSomeDamage;
+        }
+
+        //i use InitRef but you can use something like SetupInitialReferences if it helps you know what it does.. were getting all the references
+        //to things like the master script that pertains to this script...in this example its the Template_Master script.
+        void InitRef() {
+            templateMaster = GetComponent<Template_Master>();
+            //the code below is commented out because we dont actually have a player health script. but this helps paint the picture...
+            //playerHealth = GetComponent<Player_Health>().Health;
+        }
+
+        // if your going to subscribe this function, make sure it is public. remember the EventPlayerIncreaseHealth event requires a parameter. so we have to pass one in here, if you dont
+        // the subscription wont work...
+        public void DoSomeDamage(int healthDecrease) {
+            playerHealth -= healthDecrease;
+        }
+
+
+        //any functions subscribed to any single event, will all be called when the callfunction from the master script is called. so for instance, we could have a gun shoot script that when it
+        //detects collision and the collider belongs to the player, it can call the event by saying templateMaster.CallEventPlayerHealthDeduction();
+        // anything subscribed will fire off as desired. 
+
+    }
+
+}
